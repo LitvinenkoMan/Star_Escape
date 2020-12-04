472910cf0c15de209491ac73abbfa8e3c394efab
LitvinenkoMan <litvinenko.man@gmail.com>
2020-12-04 20:51:30 +0300

    туц

--- Assets/Script.cs
+++ Assets/Script.cs
@@ -7,7 +7,7 @@ public class Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Test");
     }
 
     // Update is called once per frame
