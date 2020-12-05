f5683f3afb0dbb20e3b9c8c18f9c231c77493eec
LitvinenkoMan <71625297+LitvinenkoMan@users.noreply.github.com>
2020-12-05 18:55:49 +0300

    18:54 05.12.2020
    столько всего добавленно ну нахер это описывать

---
+++ Star Escape/Assets/Scripts/EarthRotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotate : MonoBehaviour
+{
+    float dayTime;
+    IEnumerator TimeCounter()
+    {
+        while (true)
+        {
+            dayTime += Time.deltaTime;
+            if (dayTime > 1440f)
+            {
+                dayTime = 0.1f;
+            }
+
+            yield return null;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TimeCounter());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = 360.0f / 1440.0f * dayTime;
+        transform.rotation = Quaternion.Euler(0, y, 0);
+    }
+}
